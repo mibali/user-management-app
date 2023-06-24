6b2b58db6fde24ada9f7e442a132f7b17ef52324
mibali <mtbdesigns01@gmail.com>
2023-06-24 20:12:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const handleCreateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:3001/users', { name, email });
+      setName('');
+      setEmail('');
+      getUsers();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/users');
+      setUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>User Management</h1>
+      <form onSubmit={handleCreateUser}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Create User</button>
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
